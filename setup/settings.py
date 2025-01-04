7592b957274ca09f7c6ea50bbdb184f094009d0a
Jefferson Lopes <jeffbreno@gmail.com>
2025-01-04 19:09:01 -0300

    Aula DJango 40

--- setup/settings.py
+++ setup/settings.py
@@ -125,8 +125,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'recipes\media')  # Caminho absoluto para salvar os arquivos de mídia
+MEDIA_ROOT = os.path.join(BASE_DIR, 'recipes/media')  # Caminho absoluto para salvar os arquivos de mídia
